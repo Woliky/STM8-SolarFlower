73c00e458a4b48a07582446f78325d93b58d790d
Woliky <93641549+Woliky@users.noreply.github.com>
2022-03-23 21:29:45 +0100

    Add files via upload

--- proj1/main.c
+++ proj1/main.c
@@ -1,21 +1,20 @@
-// Mikroservo se polohuje pulzem šíøky 1-2ms s opakovací frekvencí pøibližnì 50Hz
-
+https://wintip.cz/602-jak-upravit-mezery-mezi-ikonami-na-plose
 #include "stm8s.h"
 #include "milis.h"
 #include "spse_stm8.h"
 #include "stm8_hd44780.h"
 #include "stdio.h"
 
-
 #define base_position 1000;
 
 void init_pwm(void);
 void servo(void);
+void ADC_init(void);
 
 
-uint16_t stop=800; 
-uint16_t time_servo=0,last_time=0;//uloženi èasu
-uint8_t x=0,b=0,y=0;//pomocné promìné 
+uint16_t stop=1000;
+uint16_t time_servo=0,last_time=0;	//uloženi èasu
+uint8_t x=0,b=0,y=0,a=0;	//pomocné promìné 
 uint8_t text[32];
 
 
@@ -24,6 +23,7 @@ CLK_HSIPrescalerConfig(CLK_PRESCALER_HSIDIV1); // 16MHz z interního RC oscilátor
 
 init_milis(); 
 init_pwm(); 
+ADC_init();
 
 GPIO_Init(GPIOD, GPIO_PIN_4, GPIO_MODE_OUT_PP_LOW_FAST);//PWM
 GPIO_Init(GPIOB, GPIO_PIN_0,GPIO_MODE_IN_PU_NO_IT);//ADC pøevod
@@ -33,40 +33,44 @@ GPIO_Init(GPIOB, GPIO_PIN_3,GPIO_MODE_IN_PU_NO_IT);//ADC pøevod
 
   while (1){
   	servo();
+		if(milis()- time >= 100){
+			L = ADC_get(ADC2_CHANNEL_2);
+			R = ADC_get(ADC2_CHANNEL_1);
+		}
+		if(R+10 > L && R-10 > L){
+			a=1;
+		}
+		if(L+10 > R && L-10 > R){
+			a=0;
+		}
   }
 }
 
 
 //zmìna DCL/støídy
+	
 void servo(void){
-	
-	if(milis() - last_time >=20 && x==0){ 
-		last_time	= milis();
-		stop=stop+3;
-		TIM2_SetCompare1(stop);
-		if(stop>=2500){	
-			x=1;
+		if(a==0){
+			TIM2_SetCompare1(1430); 
 		}
-	}
-	
-	if(milis() - last_time >=20 && x==1){ 
-		last_time	= milis();
-		stop=stop-5;
-		TIM2_SetCompare1(stop);
-		if(stop<=750){	
-			x=0;
+		if(a==1){
+			TIM2_SetCompare1(1551); 
 		}
-	}
-}
+		if(x==4){
+			TIM2_SetCompare1(0); 
+		}
+	}	
+}	
+
 
 void init_pwm(void){
 	
-TIM2_TimeBaseInit(TIM2_PRESCALER_16,20000);
+TIM2_TimeBaseInit(TIM2_PRESCALER_16,19999);
 
 TIM2_OC1Init( 	// inicializujeme kanál 1 (TM2_CH1)
 	TIM2_OCMODE_PWM1, 				// režim PWM1
 	TIM2_OUTPUTSTATE_ENABLE,	// Výstup povolen (TIMer ovládá pin)
-	800,		// výchozí hodnota šíøky pulzu je 1.5ms
+	0,		// výchozí hodnota šíøky pulzu je 1.5ms
 	TIM2_OCPOLARITY_HIGH			// Zátìž rozsvìcíme hodnotou HIGH 
 	);
 	
