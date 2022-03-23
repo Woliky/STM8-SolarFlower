76170ba7fa62135b96ca9e195bd0984cfb631b4d
Woliky <93641549+Woliky@users.noreply.github.com>
2022-03-23 21:46:03 +0100

    Add files via upload

--- proj1/main.c
+++ proj1/main.c
@@ -1,20 +1,23 @@
-#include stm8s.h
-#include milis.h
-#include stm8_hd44780.h	knihovna LCD
-#include stdio.h		
-#include spse_stm8.h		knihovno ADC
-#include emoji.h				knihovna znakù
+#include "stm8s.h"
+#include "milis.h"
+#include "stm8_hd44780.h"	//knihovna LCD
+#include "stdio.h"		
+#include "spse_stm8.h"		//knihovno ADC
+#include "emoji.h"				//knihovna znakù
+#include "stm8s_awu.h"
+#include "stm8s_exti.h"
 
 #define stav_display 1
 #define stav_menu 2
 #define stav_manual 3
+#define stav_sleep 4
 
 #define nic 1
 #define pravo 2
 #define levo 3
 #define presun 4
 
-pøeházet funkce
+//pøeházet funkce
 void process_enc(void);
 void servo_manual(void);
 void servo_auto(void);
@@ -36,25 +39,38 @@ uint8_t stav=1,stav_servo=1,block=0,strana=0,gate=0,blokada=0,volno=0,pocitadlo=
 volatile int16_t encoder=0;
 char text[24];
 
+INTERRUPT_HANDLER(AWU_IRQHandler, 1)
+{
+	AWU_GetFlagStatus();
+  GPIO_WriteReverse(GPIOC, GPIO_PIN_5);
+	stav=1;
+}
 
 void main(void){
-CLK_HSIPrescalerConfig(CLK_PRESCALER_HSIDIV1);  16MHz z interního RC oscilátoru
+CLK_HSIPrescalerConfig(CLK_PRESCALER_HSIDIV1); // 16MHz z interního RC oscilátoru
 
-init_milis();  milis kvuli delay_ms()
-lcd_init();		 inicializace displeje
-init_pwm();		inicializace PWM
-ADC_init();		inicializace ADC
+init_milis(); // milis kvuli delay_ms()
+lcd_init();		// inicializace displeje
+init_pwm();		//inicializace PWM
+ADC_init();		//inicializace ADC
 
-inicializace vstupù
-GPIO_Init(GPIOB,GPIO_PIN_0,GPIO_MODE_IN_PU_NO_IT);tlaèítko
-GPIO_Init(GPIOB,GPIO_PIN_1,GPIO_MODE_IN_PU_NO_IT);optická brána
-GPIO_Init(GPIOB,GPIO_PIN_2,GPIO_MODE_IN_PU_NO_IT);encoder kanál 1
-GPIO_Init(GPIOB,GPIO_PIN_3,GPIO_MODE_IN_PU_NO_IT);encoder kanál 2
-GPIO_Init(GPIOB,GPIO_PIN_6,GPIO_MODE_IN_PU_NO_IT);fotorezistor L ADC
-GPIO_Init(GPIOB,GPIO_PIN_7,GPIO_MODE_IN_PU_NO_IT);fotorezistor R ADC
-GPIO_Init(GPIOE,GPIO_PIN_1,GPIO_MODE_OUT_PP_LOW_SLOW);servo-pwm
 
-Uložení custom symbolù do RAM
+EXTI_SetExtIntSensitivity(EXTI_PORT_GPIOC, EXTI_SENSITIVITY_FALL_ONLY);
+AWU_LSICalibrationConfig(110000);
+AWU_Init(AWU_TIMEBASE_12S);
+
+
+//inicializace vstupù
+GPIO_Init(GPIOB,GPIO_PIN_0,GPIO_MODE_IN_PU_NO_IT);//tlaèítko
+GPIO_Init(GPIOB,GPIO_PIN_1,GPIO_MODE_IN_PU_NO_IT);//optická brána
+GPIO_Init(GPIOB,GPIO_PIN_2,GPIO_MODE_IN_PU_NO_IT);//encoder kanál 1
+GPIO_Init(GPIOB,GPIO_PIN_3,GPIO_MODE_IN_PU_NO_IT);//encoder kanál 2
+GPIO_Init(GPIOB,GPIO_PIN_6,GPIO_MODE_IN_PU_NO_IT);//fotorezistor L ADC
+GPIO_Init(GPIOB,GPIO_PIN_7,GPIO_MODE_IN_PU_NO_IT);//fotorezistor R ADC
+GPIO_Init(GPIOE,GPIO_PIN_1,GPIO_MODE_OUT_PP_LOW_SLOW);//servo-pwm
+GPIO_Init(GPIOC,GPIO_PIN_5,GPIO_MODE_OUT_PP_LOW_SLOW);
+
+//Uložení custom symbolù do RAM
 lcd_store_symbol(0,time);
 lcd_store_symbol(3,battery);
 lcd_store_symbol(1,battery_low);
@@ -65,18 +81,20 @@ lcd_store_symbol(6,cross);
 lcd_store_symbol(7,point);
 
 lcd_clear();
+enableInterrupts();
 
   while (1){
 		switch(stav){
-			stav-Zobrazování hodnot na display
-			case stav_display
+			//stav-Zobrazování hodnot na display
+			case stav_display:
 			GPIO_WriteHigh(GPIOE, GPIO_PIN_1);
+				power();
 				voltage();
-				první slot pro baterii
+				//první slot pro baterii
 				if(slot1 == 0){
 					zakaz=0;
 					volno=0;
-					sprintf(text,%s,chybi baterie);
+					sprintf(text,"%s","chybi baterie");
 					lcd_gotoxy(3,0); 
 					lcd_puts(text);
 					
@@ -87,17 +105,17 @@ lcd_clear();
 				}else{
 					lcd_sloupec=0;
 					blick_bat();
-					sprintf(text,Nabito %u,y); 
-					strcat(text, %);
+					sprintf(text,"Nabito %u",y); 
+					strcat(text, "%");
 					lcd_gotoxy(1,0); 
 					lcd_puts(text);
 					
 						
 				}
 				
-				Druhý slot pro baterii
+				//Druhý slot pro baterii
 				if(slot2 == 0){
-					sprintf(text,%s,chybi baterie);
+					sprintf(text,"%s","chybi baterie");
 					lcd_gotoxy(3,1); 
 					lcd_puts(text);
 					
@@ -112,8 +130,8 @@ lcd_clear();
 						lcd_putchar(3);
 					}else{blick_bat();}
 					
-					sprintf(text,Nabito %u,x); 
-					strcat(text, %);
+					sprintf(text,"Nabito %u",x); 
+					strcat(text, "%");
 					
 					lcd_gotoxy(1,1); 
 					lcd_puts(text);
@@ -122,127 +140,133 @@ lcd_clear();
 				optic_gate();
 				break;
 				
-			stav-prochazení menu	
-			case stav_menu
-				GPIO_WriteLow(GPIOE, GPIO_PIN_1);
+			//stav-prochazení menu	
+			case stav_menu:
 				TIM2_SetCompare1(0);
 				process_enc();
-				if(encoder = 1){
-					sprintf(text,%s,Manual);
+				if(encoder <= 1){
+					sprintf(text,"%s","Manual");
 					lcd_gotoxy(1,0); 
 					lcd_puts(text);
 				
-					sprintf(text,%s,Automat);
+					sprintf(text,"%s","Automat");
 					lcd_gotoxy(1,1); 
 					lcd_puts(text);
 				}
 				if(encoder == 2){
-					sprintf(text,%s,Sleep);
+					sprintf(text,"%s","Sleep");
 					lcd_gotoxy(1,0); 
 					lcd_puts(text);
 				}
-				if(encoder = 3 ){
+				if(encoder >= 3 ){
 					encoder=2;
 				}
 				lcd_gotoxy(0,encoder); 
 				lcd_putchar(7);
 				tlacitko();
 				break;
-		stav-manuální otáèení serva		
-		case stav_manual
-			sprintf(text,pozice %u,y); 
-			strcat(text, °);
+		//stav-manuální otáèení serva		
+		case stav_manual:
+			sprintf(text,"pozice %u",y); 
+			strcat(text, "°");
 			lcd_gotoxy(1,0); 
 			lcd_puts(text);
 			tlacitko();
 			process_enc();
 			servo_manual();
 			break;
+			
+		case stav_sleep:
+			halt();
+			break;
 		}
 	}
 }
 
-funkce pro mìøení napìtí
+//funkce pro mìøení napìtí
 void voltage(void){
-	if(milis() - volt_time = 1000){
+	if(milis() - volt_time >= 1000){
 			volt_time=milis();
-			volt=(slot15)1024;hodnota jednotek volt
-			volt1=(slot15)%102410;hodnota setin volt
-			volt=(volt100)+volt1;hodnota v setinách volt(100mv)
-			y=(420-volt)104;dosazení do trojèlenky pro zisk procentuální hodnoty
+			volt=(slot1*5)/1024;//hodnota jednotek volt
+			volt1=(slot1*5)%1024/10;//hodnota setin volt
+			volt=(volt*100)+volt1;//hodnota v setinách volt(100mv)
+			y=(420-volt)*10/4;//dosazení do trojèlenky pro zisk procentuální hodnoty
 			y=100-y;
 			
-			volt=(slot25)1024;hodnota jednotek volt
-			volt1=(slot25)%102410;hodnota setin volt
-			volt=(volt100)+volt1;hodnota v setinách volt(100mv)
-			x=(420-volt)104;dosazení do trojèlenky pro zisk procentuální hodnoty
-			x=100-y;
+			volt=(slot2*5)/1024;//hodnota jednotek volt
+			volt1=(slot2*5)%1024/10;//hodnota setin volt
+			volt=(volt*100)+volt1;//hodnota v setinách volt(100mv)
+			x=(420-volt)*10/4;//dosazení do trojèlenky pro zisk procentuální hodnoty
+			x=100-x;
 			
 	}
 }
 
 void power(void){
-	if(milis()-bat_time == 1000){pocitadlo++;}
-	if(pocitadlo==30){
+	if(milis()-bat_time >= 1000){
+		pocitadlo++;
+		bat_time=milis();
+	}
+	if(pocitadlo==5){
 		GPIO_WriteLow(GPIOE, GPIO_PIN_1);
 		GPIO_WriteLow(GPIOE, GPIO_PIN_2);
 		pocitadlo=0;
 		slot1 = ADC_get(ADC2_CHANNEL_4);
 		slot2 = ADC_get(ADC2_CHANNEL_5);
 	}	
-	if(slot1 = 840){
+	if(slot1 >= 840){
 		GPIO_WriteLow(GPIOE, GPIO_PIN_1);
 		zakaz=1;
 	}
-	if(slot2 = 840){
+	if(slot2 >= 840){
 		GPIO_WriteLow(GPIOE, GPIO_PIN_2);
 	}
-	if(slot1  0 && zakaz==0){nabíjení 1
+	if(slot1 > 0 && zakaz==0){//nabíjení 1
 		volno=1;
 		GPIO_WriteLow(GPIOE,GPIO_PIN_2);
 		GPIO_WriteHigh(GPIOE,GPIO_PIN_1);
 	}
-	if(slot2  0 && volno==0){
+	if(slot2 > 0 && volno==0){
 		GPIO_WriteLow(GPIOE,GPIO_PIN_1);
 		GPIO_WriteHigh(GPIOE,GPIO_PIN_2);
 	}
 }
 
-animace blikání baterie
+//animace blikání baterie
 void blick_bat(void){
 	
 static uint16_t bat_icon_time=0;
 static uint8_t p=0;
 
-if(milis()-bat_icon_time = 500 && p==0){
+if(milis()-bat_icon_time >= 500 && p==0){
 	lcd_gotoxy(0,lcd_sloupec); 
 	lcd_putchar(1);
 	bat_icon_time=milis();
 	p=1;
 }
 
-if(milis()-bat_icon_time = 500 && p==1){
+if(milis()-bat_icon_time >= 500 && p==1){
 	lcd_gotoxy(0,lcd_sloupec); 
 	lcd_putchar(2);
 	bat_icon_time=milis();
 	p=2;
 }
 
-if(milis()-bat_icon_time = 500 && p==2){
+if(milis()-bat_icon_time >= 500 && p==2){
 	lcd_gotoxy(0,lcd_sloupec); 
 	lcd_putchar(3);
 	bat_icon_time=milis();
 	p=3;
 }
 
-if(milis()-bat_icon_time = 500 && p==3){
+if(milis()-bat_icon_time >= 500 && p==3){
 	lcd_gotoxy(0,lcd_sloupec); 
 	lcd_putchar(4);
 	bat_icon_time=milis();
 	p=4;
 }
 
-if(milis()-bat_icon_time = 500 && p==4){
+if(milis()-bat_icon_time >= 500 && p==4){
 	lcd_gotoxy(0,lcd_sloupec); 
 	lcd_putchar(5);
 	bat_icon_time=milis();
@@ -250,7 +274,7 @@ if(milis()-bat_icon_time = 500 && p==4){
 }
 }
 
-potvrzovací tlaèítko
+//potvrzovací tlaèítko
 void tlacitko(void){
 	if(GPIO_ReadInputPin(GPIOB,GPIO_PIN_0) == RESET && last==1){
 		last=0;
@@ -269,6 +293,11 @@ void tlacitko(void){
 			lcd_clear();
 			kontrola=1;
 		}
+		if(stav==2 && encoder==2 && kontrola==0){
+			stav=stav_sleep;
+			lcd_clear();
+			kontrola=1;
+		}
 		if(stav==3 && kontrola==0){
 			stav=stav_menu;
 			lcd_clear();
@@ -277,11 +306,11 @@ void tlacitko(void){
 	}
 	if(GPIO_ReadInputPin(GPIOB,GPIO_PIN_0) != RESET){
 	last = 1;
-	kontrola=0;pøeskoèí zbylé podmínky 
+	kontrola=0;//pøeskoèí zbylé podmínky 
 	}
 }
 
- vyhodnocuje stav enkodéru
+// vyhodnocuje stav enkodéru
 void process_enc(void){
 	if(GPIO_ReadInputPin(GPIOB,GPIO_PIN_2) == RESET && minule==1){
 		minule = 0; 
@@ -297,31 +326,31 @@ void process_enc(void){
 			if(stav==2){lcd_clear();}
 			if(stav==3){last_time=milis();}
 		}
-		if(encoder  0){encoder=0;}
+		if(encoder < 0){encoder=0;}
 	}
 	if(GPIO_ReadInputPin(GPIOB,GPIO_PIN_2) != RESET){minule = 1;} 
 	}
 	
-funkce pro automatické otáèení serva
+//funkce pro automatické otáèení serva
 void servo_auto(void){
-	if(milis()- foto_time = 100){
+	if(milis()- foto_time >= 100){
 		left = ADC_get(ADC2_CHANNEL_6);
 		right = ADC_get(ADC2_CHANNEL_7);
 		
-		if(right+40  left && right-40  left){do prava
+		if(right+40 > left && right-40 > left){//do prava
 			turn=1;
 		}
-		else if(left+40  right && left-40  right){do leva
+		else if(left+40 > right && left-40 > right){//do leva
 			turn=2;
 		}
 		else{
 			turn=0;
 		}
 	}
-	if(turn==1 && block==1){pravo
+	if(turn==1 && block==1){//pravo
 		TIM2_SetCompare1(1551); 
 	}
-	if(turn==2 && block==2){levo
+	if(turn==2 && block==2){//levo
 		TIM2_SetCompare1(1430); 
 	}
 	if(turn==0){
@@ -331,14 +360,14 @@ void servo_auto(void){
 }
 
 
-funkce pro obsluhu optické brány
+//funkce pro obsluhu optické brány
 void optic_gate(void){
 	gate_prevod = ADC_get(ADC2_CHANNEL_1);
-	if(gate_prevod = 300 && block==0){
+	if(gate_prevod <= 300 && block==0){
 		gate++;
 		block=1;
 	}
-	if(gate_prevod = 350){
+	if(gate_prevod >= 350){
 		block=0;
 		blokada=0;
 	}
@@ -346,25 +375,25 @@ void optic_gate(void){
 	
 	
 	switch(stav_servo){
-		case nic
+		case nic:
 			
 				left = ADC_get(ADC2_CHANNEL_6);
 				right = ADC_get(ADC2_CHANNEL_7);
 		
-				if(right+40  left && right-40  left){do prava
+				if(right+40 > left && right-40 > left){//do prava
 					turn=1;
 				}
-				else if(left+40  right && left-40  right){do leva
+				else if(left+40 > right && left-40 > right){//do leva
 					turn=2;
 				}
 				else{
 					turn=0;
 				}
 			
-			if(turn==1){pravo
+			if(turn==1){//pravo
 				TIM2_SetCompare1(1551); 
 			}
-			if(turn==2){levo
+			if(turn==2){//levo
 				TIM2_SetCompare1(1430); 
 			}
 			if(turn==0){
@@ -378,43 +407,43 @@ void optic_gate(void){
 			}
 		break;
 		
-		case pravo
+		case pravo:
 			TIM2_SetCompare1(0);
 			
 				left = ADC_get(ADC2_CHANNEL_6);
 				right = ADC_get(ADC2_CHANNEL_7);
 		
-				if(right+150  left && right-150  left){do prava
+				if(right+150 > left && right-150 > left){//do prava
 					stav_servo = presun;
 					strana=1;
 				}
-				if(left+40  right && left-40  right){do leva
+				if(left+40 > right && left-40 > right){//do leva
 					stav_servo = nic;
 					blokada=1;
 				}
 			
 			break;
 			
-		case levo
+		case levo:
 			TIM2_SetCompare1(0);
 			
 				left = ADC_get(ADC2_CHANNEL_6);
 				right = ADC_get(ADC2_CHANNEL_7);
 		
-				if(right+40  left && right-40  left){do prava
+				if(right+40 > left && right-40 > left){//do prava
 					stav_servo = nic;
 					blokada=1;
 				}
-				if(left+150  right && left-150  right){do leva
+				if(left+150 > right && left-150 > right){//do leva
 					stav_servo = presun;
 					strana=2;
 				}
 			
 			break;
 			
-		case presun
+		case presun:
 			if(strana==1){
-				TIM2_SetCompare1(1430); do leva
+				TIM2_SetCompare1(1430); //do leva
 				if(gate==2){
 					TIM2_SetCompare1(0); 
 					stav_servo = levo;
@@ -422,7 +451,7 @@ void optic_gate(void){
 				}
 			}
 			if(strana==2){
-				TIM2_SetCompare1(1551); do prava
+				TIM2_SetCompare1(1551); //do prava
 				if(gate==2){
 					TIM2_SetCompare1(0); 
 					stav_servo = pravo;
@@ -434,7 +463,7 @@ void optic_gate(void){
 }
 
 
-funkce pro manuální natoèení serva
+//funkce pro manuální natoèení serva
 void servo_manual(void){
 	if(run==1){
 		TIM2_SetCompare1(1430);
@@ -442,13 +471,13 @@ void servo_manual(void){
 	if(run==2){
 		TIM2_SetCompare1(1551);
 	}
-	if(milis() - last_time = 25){
+	if(milis() - last_time >= 25){
 			run=0;
 			TIM2_SetCompare1(0);
 	}
 }
 
-funkce pro nastavení PWM
+//funkce pro nastavení PWM
 void init_pwm(void){
 TIM2_TimeBaseInit(TIM2_PRESCALER_16,19999);
 
@@ -463,13 +492,13 @@ TIM2_OC1PreloadConfig(ENABLE);
 TIM2_Cmd(ENABLE);
 }
 
-funkce pro nastavení ADC pøevodníku
+//funkce pro nastavení ADC pøevodníku
 void ADC_init(void){
-ADC2_SchmittTriggerConfig(ADC2_SCHMITTTRIG_CHANNEL1,DISABLE);optická brána PB1
-ADC2_SchmittTriggerConfig(ADC2_SCHMITTTRIG_CHANNEL4,DISABLE);Baterie 1
-ADC2_SchmittTriggerConfig(ADC2_SCHMITTTRIG_CHANNEL5,DISABLE);Baterie 2
-ADC2_SchmittTriggerConfig(ADC2_SCHMITTTRIG_CHANNEL6,DISABLE);levý foto PB6
-ADC2_SchmittTriggerConfig(ADC2_SCHMITTTRIG_CHANNEL7,DISABLE);pravý foto PB7
+ADC2_SchmittTriggerConfig(ADC2_SCHMITTTRIG_CHANNEL1,DISABLE);//optická brána PB1
+ADC2_SchmittTriggerConfig(ADC2_SCHMITTTRIG_CHANNEL4,DISABLE);//Baterie 1
+ADC2_SchmittTriggerConfig(ADC2_SCHMITTTRIG_CHANNEL5,DISABLE);//Baterie 2
+ADC2_SchmittTriggerConfig(ADC2_SCHMITTTRIG_CHANNEL6,DISABLE);//levý foto PB6
+ADC2_SchmittTriggerConfig(ADC2_SCHMITTTRIG_CHANNEL7,DISABLE);//pravý foto PB7
 ADC2_PrescalerConfig(ADC2_PRESSEL_FCPU_D4);
 ADC2_AlignConfig(ADC2_ALIGN_RIGHT);
 ADC2_Select_Channel(ADC2_CHANNEL_6);
@@ -477,22 +506,22 @@ ADC2_Cmd(ENABLE);
 ADC2_Startup_Wait();
 }
 
- pod tímto komentáøem nic nemìòte 
+// pod tímto komentáøem nic nemìòte 
 #ifdef USE_FULL_ASSERT
 
-
-   @brief  Reports the name of the source file and the source line number
-     where the assert_param error has occurred.
-   @param file pointer to the source file name
-   @param line assert_param error line source number
-   @retval  None
-  
-void assert_failed(u8 file, u32 line)
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *   where the assert_param error has occurred.
+  * @param file: pointer to the source file name
+  * @param line: assert_param error line source number
+  * @retval : None
+  */
+void assert_failed(u8* file, u32 line)
 { 
-   User can add his own implementation to report the file name and line number,
-     ex printf(Wrong parameters value file %s on line %drn, file, line) 
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
 
-   Infinite loop 
+  /* Infinite loop */
   while (1)
   {
   }
